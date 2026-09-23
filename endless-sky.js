d9846a0f9d22e3c274c642701547e937cc3ac9e6
Isaac4655 <157089689+Isaac4655@users.noreply.github.com>
2026-09-23 17:25:48 -0500

    Did some thread stuff.

--- endless-sky.js
+++ endless-sky.js
@@ -89,7 +89,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\Isaac\AppData\Local\Temp\tmp4wp9zluz.js
+// include: C:\Users\Isaac\AppData\Local\Temp\tmp7x82303s.js
 if (!Module["expectedDataFileDownloads"]) Module["expectedDataFileDownloads"] = 0;
 
 Module["expectedDataFileDownloads"]++;
@@ -228,23 +228,23 @@ Module["expectedDataFileDownloads"]++;
   });
 })();
 
-// end include: C:\Users\Isaac\AppData\Local\Temp\tmp4wp9zluz.js
-// include: C:\Users\Isaac\AppData\Local\Temp\tmpy5zxy5zv.js
+// end include: C:\Users\Isaac\AppData\Local\Temp\tmp7x82303s.js
+// include: C:\Users\Isaac\AppData\Local\Temp\tmprjhyqmn0.js
 // All the pre-js content up to here must remain later on, we need to run
 // it.
 if ((typeof ENVIRONMENT_IS_WASM_WORKER != "undefined" && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != "undefined" && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != "undefined" && ENVIRONMENT_IS_AUDIO_WORKLET)) Module["preRun"] = [];
 
 var necessaryPreJSTasks = Module["preRun"].slice();
 
-// end include: C:\Users\Isaac\AppData\Local\Temp\tmpy5zxy5zv.js
-// include: C:\Users\Isaac\AppData\Local\Temp\tmpzox0fscp.js
+// end include: C:\Users\Isaac\AppData\Local\Temp\tmprjhyqmn0.js
+// include: C:\Users\Isaac\AppData\Local\Temp\tmpd6wu3c8a.js
 if (!Module["preRun"]) throw "Module.preRun should exist because file support used it; did a pre-js delete it?";
 
 necessaryPreJSTasks.forEach(task => {
   if (Module["preRun"].indexOf(task) < 0) throw "All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?";
 });
 
-// end include: C:\Users\Isaac\AppData\Local\Temp\tmpzox0fscp.js
+// end include: C:\Users\Isaac\AppData\Local\Temp\tmpd6wu3c8a.js
 var programArgs = [];
 
 var thisProgram = "./this.program";
