79e04a4dfd3686be9c903429eeafc48f79f5534f
Isaac4655 <157089689+Isaac4655@users.noreply.github.com>
2026-09-23 19:31:03 -0500

    Still... fixing.

--- endless-sky.js
+++ endless-sky.js
@@ -13673,8 +13673,8 @@ function checkIncomingModuleAPI() {
 }
 
 var ASM_CONSTS = {
-  389693: () => (growMemViews(), HEAPU8).length,
-  389719: $0 => {
+  389773: () => (growMemViews(), HEAPU8).length,
+  389799: $0 => {
     var str = UTF8ToString($0) + "\n\n" + "Abort/Retry/Ignore/AlwaysIgnore? [ariA] :";
     var reply = window.prompt(str, "i");
     if (reply === null) {
@@ -13682,7 +13682,7 @@ var ASM_CONSTS = {
     }
     return reply.length === 1 ? reply.charCodeAt(0) : -1;
   },
-  389934: () => {
+  390014: () => {
     if (typeof (AudioContext) !== "undefined") {
       return true;
     } else if (typeof (webkitAudioContext) !== "undefined") {
@@ -13690,7 +13690,7 @@ var ASM_CONSTS = {
     }
     return false;
   },
-  390081: () => {
+  390161: () => {
     if ((typeof (navigator.mediaDevices) !== "undefined") && (typeof (navigator.mediaDevices.getUserMedia) !== "undefined")) {
       return true;
     } else if (typeof (navigator.webkitGetUserMedia) !== "undefined") {
@@ -13698,7 +13698,7 @@ var ASM_CONSTS = {
     }
     return false;
   },
-  390315: $0 => {
+  390395: $0 => {
     if (typeof (Module["SDL2"]) === "undefined") {
       Module["SDL2"] = {};
     }
@@ -13722,11 +13722,11 @@ var ASM_CONSTS = {
     }
     return SDL2.audioContext === undefined ? -1 : 0;
   },
-  390867: () => {
+  390947: () => {
     var SDL2 = Module["SDL2"];
     return SDL2.audioContext.sampleRate;
   },
-  390935: ($0, $1, $2, $3) => {
+  391015: ($0, $1, $2, $3) => {
     var SDL2 = Module["SDL2"];
     var have_microphone = function(stream) {
       if (SDL2.capture.silenceTimer !== undefined) {
@@ -13768,7 +13768,7 @@ var ASM_CONSTS = {
       }, have_microphone, no_microphone);
     }
   },
-  392628: ($0, $1, $2, $3) => {
+  392708: ($0, $1, $2, $3) => {
     var SDL2 = Module["SDL2"];
     SDL2.audio.scriptProcessorNode = SDL2.audioContext["createScriptProcessor"]($1, 0, $0);
     SDL2.audio.scriptProcessorNode["onaudioprocess"] = function(e) {
@@ -13800,7 +13800,7 @@ var ASM_CONSTS = {
       SDL2.audio.silenceTimer = setInterval(silence_callback, ($1 / SDL2.audioContext.sampleRate) * 1e3);
     }
   },
-  393803: ($0, $1) => {
+  393883: ($0, $1) => {
     var SDL2 = Module["SDL2"];
     var numChannels = SDL2.capture.currentCaptureBuffer.numberOfChannels;
     for (var c = 0; c < numChannels; ++c) {
@@ -13819,7 +13819,7 @@ var ASM_CONSTS = {
       }
     }
   },
-  394408: ($0, $1) => {
+  394488: ($0, $1) => {
     var SDL2 = Module["SDL2"];
     var buf = $0 >>> 2;
     var numChannels = SDL2.audio.currentOutputBuffer["numberOfChannels"];
@@ -13833,7 +13833,7 @@ var ASM_CONSTS = {
       }
     }
   },
-  394897: $0 => {
+  394977: $0 => {
     var SDL2 = Module["SDL2"];
     if ($0) {
       if (SDL2.capture.silenceTimer !== undefined) {
@@ -13867,10 +13867,10 @@ var ASM_CONSTS = {
       SDL2.audioContext = undefined;
     }
   },
-  395903: $0 => {
+  395983: $0 => {
     window.open(UTF8ToString($0), "_blank");
   },
-  395943: ($0, $1, $2) => {
+  396023: ($0, $1, $2) => {
     var w = $0;
     var h = $1;
     var pixels = $2;
@@ -13941,7 +13941,7 @@ var ASM_CONSTS = {
     }
     SDL2.ctx.putImageData(SDL2.image, 0, 0);
   },
-  397409: ($0, $1, $2, $3, $4) => {
+  397489: ($0, $1, $2, $3, $4) => {
     var w = $0;
     var h = $1;
     var hot_x = $2;
@@ -13978,18 +13978,18 @@ var ASM_CONSTS = {
     stringToUTF8(url, urlBuf, url.length + 1);
     return urlBuf;
   },
-  398397: $0 => {
+  398477: $0 => {
     if (Module["canvas"]) {
       Module["canvas"].style["cursor"] = UTF8ToString($0);
     }
   },
-  398480: () => {
+  398560: () => {
     if (Module["canvas"]) {
       Module["canvas"].style["cursor"] = "none";
     }
   },
-  398549: () => window.innerWidth,
-  398579: () => window.innerHeight
+  398629: () => window.innerWidth,
+  398659: () => window.innerHeight
 };
 
 function InitializeWebLazyResources() {
