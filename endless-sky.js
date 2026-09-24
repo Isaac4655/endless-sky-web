1ee191037d991e6434445a7c1510fe5d5430688d
Isaac4655 <157089689+Isaac4655@users.noreply.github.com>
2026-09-23 21:51:44 -0500

    reduced threads

--- endless-sky.js
+++ endless-sky.js
@@ -89,7 +89,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\Isaac\AppData\Local\Temp\tmpbzewdyet.js
+// include: C:\Users\Isaac\AppData\Local\Temp\tmpsf2dsjie.js
 if (!Module["expectedDataFileDownloads"]) Module["expectedDataFileDownloads"] = 0;
 
 Module["expectedDataFileDownloads"]++;
@@ -228,23 +228,23 @@ Module["expectedDataFileDownloads"]++;
   });
 })();
 
-// end include: C:\Users\Isaac\AppData\Local\Temp\tmpbzewdyet.js
-// include: C:\Users\Isaac\AppData\Local\Temp\tmp7v9geq99.js
+// end include: C:\Users\Isaac\AppData\Local\Temp\tmpsf2dsjie.js
+// include: C:\Users\Isaac\AppData\Local\Temp\tmp8j47ug30.js
 // All the pre-js content up to here must remain later on, we need to run
 // it.
 if ((typeof ENVIRONMENT_IS_WASM_WORKER != "undefined" && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != "undefined" && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != "undefined" && ENVIRONMENT_IS_AUDIO_WORKLET)) Module["preRun"] = [];
 
 var necessaryPreJSTasks = Module["preRun"].slice();
 
-// end include: C:\Users\Isaac\AppData\Local\Temp\tmp7v9geq99.js
-// include: C:\Users\Isaac\AppData\Local\Temp\tmpoah4x1qg.js
+// end include: C:\Users\Isaac\AppData\Local\Temp\tmp8j47ug30.js
+// include: C:\Users\Isaac\AppData\Local\Temp\tmppx_ow2kj.js
 if (!Module["preRun"]) throw "Module.preRun should exist because file support used it; did a pre-js delete it?";
 
 necessaryPreJSTasks.forEach(task => {
   if (Module["preRun"].indexOf(task) < 0) throw "All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?";
 });
 
-// end include: C:\Users\Isaac\AppData\Local\Temp\tmpoah4x1qg.js
+// end include: C:\Users\Isaac\AppData\Local\Temp\tmppx_ow2kj.js
 var programArgs = [];
 
 var thisProgram = "./this.program";
@@ -1358,7 +1358,7 @@ var PThread = {
     }
   },
   initMainThread() {
-    var pthreadPoolSize = 12;
+    var pthreadPoolSize = 4;
     // Start loading up the Worker pool, if requested.
     while (pthreadPoolSize--) {
       PThread.allocateUnusedWorker();
