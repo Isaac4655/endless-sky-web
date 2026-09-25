343cfd3e4b778b45ad5676fbe313c68a676508dd
Isaac4655 <157089689+Isaac4655@users.noreply.github.com>
2026-09-24 19:25:19 -0500

    Narrowing down the logs.

--- endless-sky.js
+++ endless-sky.js
@@ -13372,8 +13372,8 @@ function checkIncomingModuleAPI() {
 }
 
 var ASM_CONSTS = {
-  393515: () => (growMemViews(), HEAPU8).length,
-  393541: $0 => {
+  396171: () => (growMemViews(), HEAPU8).length,
+  396197: $0 => {
     var str = UTF8ToString($0) + "\n\n" + "Abort/Retry/Ignore/AlwaysIgnore? [ariA] :";
     var reply = window.prompt(str, "i");
     if (reply === null) {
@@ -13381,7 +13381,7 @@ var ASM_CONSTS = {
     }
     return reply.length === 1 ? reply.charCodeAt(0) : -1;
   },
-  393756: () => {
+  396412: () => {
     if (typeof (AudioContext) !== "undefined") {
       return true;
     } else if (typeof (webkitAudioContext) !== "undefined") {
@@ -13389,7 +13389,7 @@ var ASM_CONSTS = {
     }
     return false;
   },
-  393903: () => {
+  396559: () => {
     if ((typeof (navigator.mediaDevices) !== "undefined") && (typeof (navigator.mediaDevices.getUserMedia) !== "undefined")) {
       return true;
     } else if (typeof (navigator.webkitGetUserMedia) !== "undefined") {
@@ -13397,7 +13397,7 @@ var ASM_CONSTS = {
     }
     return false;
   },
-  394137: $0 => {
+  396793: $0 => {
     if (typeof (Module["SDL2"]) === "undefined") {
       Module["SDL2"] = {};
     }
@@ -13421,11 +13421,11 @@ var ASM_CONSTS = {
     }
     return SDL2.audioContext === undefined ? -1 : 0;
   },
-  394689: () => {
+  397345: () => {
     var SDL2 = Module["SDL2"];
     return SDL2.audioContext.sampleRate;
   },
-  394757: ($0, $1, $2, $3) => {
+  397413: ($0, $1, $2, $3) => {
     var SDL2 = Module["SDL2"];
     var have_microphone = function(stream) {
       if (SDL2.capture.silenceTimer !== undefined) {
@@ -13467,7 +13467,7 @@ var ASM_CONSTS = {
       }, have_microphone, no_microphone);
     }
   },
-  396450: ($0, $1, $2, $3) => {
+  399106: ($0, $1, $2, $3) => {
     var SDL2 = Module["SDL2"];
     SDL2.audio.scriptProcessorNode = SDL2.audioContext["createScriptProcessor"]($1, 0, $0);
     SDL2.audio.scriptProcessorNode["onaudioprocess"] = function(e) {
@@ -13499,7 +13499,7 @@ var ASM_CONSTS = {
       SDL2.audio.silenceTimer = setInterval(silence_callback, ($1 / SDL2.audioContext.sampleRate) * 1e3);
     }
   },
-  397625: ($0, $1) => {
+  400281: ($0, $1) => {
     var SDL2 = Module["SDL2"];
     var numChannels = SDL2.capture.currentCaptureBuffer.numberOfChannels;
     for (var c = 0; c < numChannels; ++c) {
@@ -13518,7 +13518,7 @@ var ASM_CONSTS = {
       }
     }
   },
-  398230: ($0, $1) => {
+  400886: ($0, $1) => {
     var SDL2 = Module["SDL2"];
     var buf = $0 >>> 2;
     var numChannels = SDL2.audio.currentOutputBuffer["numberOfChannels"];
@@ -13532,7 +13532,7 @@ var ASM_CONSTS = {
       }
     }
   },
-  398719: $0 => {
+  401375: $0 => {
     var SDL2 = Module["SDL2"];
     if ($0) {
       if (SDL2.capture.silenceTimer !== undefined) {
@@ -13566,10 +13566,10 @@ var ASM_CONSTS = {
       SDL2.audioContext = undefined;
     }
   },
-  399725: $0 => {
+  402381: $0 => {
     window.open(UTF8ToString($0), "_blank");
   },
-  399765: ($0, $1, $2) => {
+  402421: ($0, $1, $2) => {
     var w = $0;
     var h = $1;
     var pixels = $2;
@@ -13640,7 +13640,7 @@ var ASM_CONSTS = {
     }
     SDL2.ctx.putImageData(SDL2.image, 0, 0);
   },
-  401231: ($0, $1, $2, $3, $4) => {
+  403887: ($0, $1, $2, $3, $4) => {
     var w = $0;
     var h = $1;
     var hot_x = $2;
@@ -13677,18 +13677,18 @@ var ASM_CONSTS = {
     stringToUTF8(url, urlBuf, url.length + 1);
     return urlBuf;
   },
-  402219: $0 => {
+  404875: $0 => {
     if (Module["canvas"]) {
       Module["canvas"].style["cursor"] = UTF8ToString($0);
     }
   },
-  402302: () => {
+  404958: () => {
     if (Module["canvas"]) {
       Module["canvas"].style["cursor"] = "none";
     }
   },
-  402371: () => window.innerWidth,
-  402401: () => window.innerHeight
+  405027: () => window.innerWidth,
+  405057: () => window.innerHeight
 };
 
 function WebResourceCategoryList(category) {
